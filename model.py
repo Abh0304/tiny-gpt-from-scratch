da99441baceeadbda27d60471e354fff7580a74a
Mavis <147696697+Abh0304@users.noreply.github.com>
2026-09-17 07:36:07 +0530

    Step 2: build_stoi

--- model.py
+++ model.py
@@ -15,8 +15,13 @@ def build_vocab(text):
     return sorted_char
     pass
 
-# Step 2 - build_stoi (not yet solved)
-# TODO: implement
+# Step 2 - build_stoi
+def build_stoi(vocab):
+    """Return a dict mapping each character in vocab to its index."""
+    # TODO: map each character in vocab to its integer position
+    stoi = {ch: i for i, ch in enumerate(vocab)}
+    return stoi
+    pass
 
 # Step 3 - build_itos (not yet solved)
 # TODO: implement
